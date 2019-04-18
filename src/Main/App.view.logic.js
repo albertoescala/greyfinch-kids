86a001652ebe3f1b793fbe038a74c163c328b451
Alberto Escala <albertoaesc@gmail.com>
2019-04-17 23:13:31 -0500

    feat(package.json): add react-router

--- src/Main/App.view.logic.js
+++ src/Main/App.view.logic.js
@@ -1,7 +1,10 @@
 import App from './App.view.js'
 import React from 'react'
+import Button from '../Button/Button.view.js'
 
 let AppLogic = props => {
-  return <App {...props} />
+  return <App {...props}>
+  <Button text="Start" onClick={() => console.log('clicked')} />
+  </App>
 }
 export default AppLogic
