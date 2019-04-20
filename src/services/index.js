edf4034c9e88a6f8d14275d865e4001691c9c055
Alberto Escala <albertoaesc@gmail.com>
2019-04-20 10:00:18 -0500

    feat(.env): add .env file

--- src/services/index.js
+++ src/services/index.js
@@ -4,7 +4,7 @@ export const searchGiphy = (search, limit) => {
   try {
     const response = await fetchData({
       method: 'get', // eslint-disable-next-line
-      url: `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${process.env.API_GIPHY}&limit=${limit || 1}`,
+      url: `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${process.env.REACT_APP_API_GIPHY}&limit=${limit || 1}`,
       headers: {
         'Content-Type': 'application/json'
       }
